5865052252b1f865a84e0638a4eb185411ab0fa1
Jason Creighton & Kenny Grage <student@epicodus.com>
2015-08-17 13:34:02 -0700

    pass 8 tests

---
+++ tests/CategoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Category.php";
+
+    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CategoryTest extends PHPUnit_Framework_TestCase {
+
+
+        protected function tearDown() {
+            Category::deleteAll();
+        }
+
+        function test_getName() {
+
+            //Arrange
+            $name = "Work stuff";
+            $test_Category = new Category($name);
+
+            //Act
+            $result = $test_Category->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId() {
+            //Arrange
+            $name = "Work stuff";
+            $id = 1;
+            $test_Category = new Category($name, $id);
+
+            //Act
+            $result = $test_Category->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save() {
+            //Arrange
+            $name = "Work stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+
+            //Act
+            $result = Category::getAll();
+
+            //Assert
+            $this->assertEquals($test_Category, $result[0]);
+        }
+    }
+
+?>
